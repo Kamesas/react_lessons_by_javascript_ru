b8043fe80d6596c63e4af2cc524c8ea306ec7565
Alexandr <semakaleksandr2014@gmail.com>
2018-06-02 13:51:58 +0300

    added App.js & Article.js

---
+++ src/components/Article.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Article () {
+ 	const body = <section>body</section>
+	return(
+    	<div>
+      		<h2>Babel</h2>
+					{body}
+      	</div>
+    )
+}
+
+export default Article
