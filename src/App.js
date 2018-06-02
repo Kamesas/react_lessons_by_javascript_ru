fd1def354629c8fc3d14a59db41e839a540be133
Alexandr <semakaleksandr2014@gmail.com>
2018-06-02 12:45:07 +0300

    added SemanticUI

--- src/App.js
+++ src/App.js
@@ -13,6 +13,24 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
           <p><strong>Yes, it's WKORK!!!</strong></p>
+
+          <div className="SemanticUI">
+            <p><strong>Semantic UI</strong> </p>
+            <div className="ui icon buttons">
+              <button className="ui button">
+                <i className="play icon"></i>
+              </button>
+              <button className="ui button">
+                <i className="pause icon"></i>
+              </button>
+              <button className="ui button">
+                <i className="shuffle icon"></i>
+              </button>
+            </div>
+          </div>
+
+          
+
         </p> 
       </div>
     );
