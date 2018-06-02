61a0058d509d3252c3bd2128cd8e5bd457c199a7
Alexandr <semakaleksandr2014@gmail.com>
2018-06-02 14:57:40 +0300

    added fixtures.js & brought articles on screen

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Article from './Article'
+import fixtures from '../fixtures'
 
 function App () {
 	return (
 			<div>
-				<h1>App name second</h1>
-				<Article />
+				<h1>App name</h1>
+				<Article article={fixtures[0]} />
 			</div>
 		) 
 }
--- src/components/Article.js
+++ src/components/Article.js
@@ -1,13 +1,31 @@
 import React from 'react'
 
-function Article () {
- 	const body = <section>body</section>
+function Article (props) { //все данные приходят в props
+
+	const {article} = props //дуструктуризация см. ниже
+ 	const body = <section>{article.text}</section>
+
 	return(
     	<div>
-      		<h2>Babel</h2>
+      		<h2>{article.title}</h2>
 					{body}
+					<h3>creation date: {(new Date(article.date)).toDateString()}</h3>
       	</div>
     )
+
 }
 
 export default Article
+
+//позволяет пирсвоить объект или массив сразу нескольким переменным, разбив его на части https://learn.javascript.ru/destructuring
+// let options = {
+//   title: "Меню",
+//   width: 100,
+//   height: 200
+// };
+
+// let {title, width, height} = options;
+
+//alert(title);  // Меню
+//alert(width);  // 100
+//alert(height); // 200
