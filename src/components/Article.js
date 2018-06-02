187c4865079a0d1b3cf116a9aed5dbf1839b01f9
Alexandr <semakaleksandr2014@gmail.com>
2018-06-02 16:35:13 +0300

    added function for hide text and added experemental syntax

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,24 +1,45 @@
 import React, {Component} from 'react'
 
 class Article extends Component {
+
+	// constructor(props) {
+	//     super(props);
+	// 	    this.state = {isOpen: true, };	    	    
+	// 	   	this.handleClick = handleClick.bind(this) //<button onClick={this.handleClick} >close</button>
+ //  };
+  //or simlple
+  state = {
+  	isOpen: true
+  }
+
+  handleClick = () => {
+		console.log('---', 'clicked')
+		this.setState({
+			isOpen: !this.state.isOpen
+		})
+	}
+
 	render() {
 
 		const {article} = this.props 
-	 	const body = <section>{article.text}</section>
+	 	const body = this.state.isOpen && <section>{article.text}</section>
 		return(
+
 	    	<div>
-	      		<h2>{article.title}</h2>
-	      		<button onClick={handleClick} >close</button>
-						{body}
-						<h3>creation date: {(new Date(article.date)).toDateString()}</h3>
-	      	</div>
-	    )
+      		<h2>{article.title}</h2>
+      		<button onClick={this.handleClick} >{this.state.isOpen ? 'close' : 'open'}</button>
+      		<hr/>
+					{body}
+					<h3>creation date: {(new Date(article.date)).toDateString()}</h3>
+      	</div>
 
+	    )		
 	}
+
 }
 
-function handleClick () {
-	console.log('---', 'clicked')
-}
+// function handleClick () {
+// 	console.log('---', 'clicked')
+// }
 
 export default Article
